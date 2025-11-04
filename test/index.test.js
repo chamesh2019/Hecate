7e52b7f0205269a0fd7e978bcf8b887b05608645
Chames Dinuka <101823341+chamesh2019@users.noreply.github.com>
2025-11-04 22:56:52 +0530

    Refactor CI workflow and tests for Hecate
    - Updated CI workflow to use a matrix strategy for Node.js versions.
    - Changed Node.js setup action to v4 and adjusted node-version configuration.
    - Modified test cases to utilize environment variable for HECATE_API_KEY instead of hardcoded value.
    - Updated supabase dependency versioning in package.json for consistency.

--- test/index.test.js
+++ test/index.test.js
@@ -2,15 +2,17 @@ import Hecate from '../src/index.js';
 
 describe('Hecate initialization', () => {
     it('should throw an error if environment variables are not set', () => {
+        let key = process.env.HECATE_API_KEY;
         process.env.HECATE_API_KEY = '';
 
         expect(() => new Hecate()).toThrowError('HECATE_API_KEY required');
+        process.env.HECATE_API_KEY = key;
     });
 });
 
 describe('Hecate getSecret', () => {
     it('should fetch secret successfully when valid secret name is provided', async () => {
-        const hecate = new Hecate('hk_fdb846d720ce9ec2133e091fa1c72754d01595d8201efd97a555c7ff9744bcba');
+        const hecate = new Hecate(process.env.HECATE_API_KEY);
 
         const secret = await hecate.getSecret('test-secret');
 
